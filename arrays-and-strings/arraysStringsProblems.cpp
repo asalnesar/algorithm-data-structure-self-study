e03ebf52b8a3b662559e276dc7c8834ae11287a4
Asal Nesar Noubari <a.nesarnobari@gmail.com>
2022-03-09 01:30:46 +0330

    Urlify

--- arrays-and-strings/arraysStringsProblems.cpp
+++ arrays-and-strings/arraysStringsProblems.cpp
@@ -51,6 +51,43 @@ bool checkPermutation2(string str1, string str2)
     return true;
 }
 
+int countSpace(string str, int start, int end, int target)
+{
+    int count = 0;
+    for(int i= start ; i< end; i++){
+        if(str.at(i) == target)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string urlify(string str, int trueLength)
+{
+    int numberOfSpaces =  countSpace(str, 0, trueLength, ' ');
+    int newIndex = trueLength - 1 + (numberOfSpaces * 2);
+    cout <<"number of spaces: " << numberOfSpaces << endl;
+    cout <<"new index: "<<newIndex <<endl;
+    if(newIndex + 1 < str.length()) {
+        str.at(newIndex+1)='\0';
+    }
+    for(int i = trueLength - 1; i >=0 ; i--)
+    {
+        if(str.at(i) == ' '){
+            str.at(newIndex) = '0';
+            str.at(newIndex - 1) = '2';
+            str.at(newIndex  - 2) = '%';
+            newIndex -= 3;
+        }
+        else{
+            str.at(newIndex) = str.at(i);
+            newIndex--;
+        }
+    }
+    return str;
+}
+
 void  testIsUniques()
 {
     cout << "Check if a string has unique characters." << endl;
@@ -73,9 +110,14 @@ void  testPermutation()
     cout << "for string " << str1 << " and string " << str2 << " result is " << result << endl;
 }
 
+void testUrlify()
+{
+    cout << urlify("asal nesar nobari     ", 17) << endl;
+}
+
 int main()
 {
-    testPermutation();
+    testUrlify();
     return 0;
 }
 
