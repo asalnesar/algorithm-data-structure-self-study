6e2ea778c07bae0c81f9134b60445aa9e658e601
Asal Nesar Noubari <a.nesarnobari@gmail.com>
2022-03-14 16:06:42 +0330

    Sum list forward

--- 2-linked-lists/LinkedListProblems.cpp
+++ 2-linked-lists/LinkedListProblems.cpp
@@ -151,6 +151,55 @@ Node* sumList(Node *n, Node *m){
     return result;
 }
 
+int length(Node *n){
+    int count = 0;
+    while(n!=NULL){
+        count++;
+        n=n->next;
+    }
+    return count;
+}
+
+Node* insertBefore(Node *n, int data){
+    Node *head = new Node(data);
+    if(n != NULL){
+        head->next = n;
+    }
+    return head;
+}
+
+Node* padNode(Node *n, int padding){
+    Node *head = n;
+    for(int i = 0;i<padding;i++){
+        head = insertBefore(head, 0);
+    }
+    return head;
+}
+Node* addHelper(Node *n, Node *m, int &carry){
+    if(n == NULL && m == NULL) return NULL;
+    Node *sum = addHelper(n->next, m->next, carry);
+    int val = n->data  + m->data + carry;
+    Node *fullResult = insertBefore(sum, val%10);
+    carry = (val - val%10)/10;
+    return fullResult;
+}
+Node* sumListForward(Node *n, Node*m){
+    int len1 = length(n);
+    int len2 = length(m);
+    int carry = 0;
+    if(len1 > len2){
+        m= padNode(m, len1-len2);
+    } 
+    else if(len2 > len1){
+        n = padNode(n, len2-len1);
+    }
+    Node *result = addHelper(n,m,carry);
+    if(carry != 0){
+        result = insertBefore(result, carry);
+    }
+    return result;
+}
+
 
 void testRemoveDups(){
     Node *head= new Node(1);
@@ -225,8 +274,19 @@ void testSumList(){
     Node *result = sumList(n,m);
     result->printLinkedList();
 }
-
+void testSumListForward(){
+    Node *n = new Node(6);
+    n->appendToTail(1);
+    n->appendToTail(7);
+    n->printLinkedList();
+    Node *m = new Node(2);
+    m->appendToTail(9);
+    m->appendToTail(5);
+    m->printLinkedList();
+    Node *result = sumListForward(n,m);
+    result->printLinkedList();
+}
 int main(){
-    testSumList();
+    testSumListForward();
     return 0;
 }
