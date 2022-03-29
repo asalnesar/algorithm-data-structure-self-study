98171f6a565c2b94535abae3ee92a5a5ece3f33c
Asal Nesar Noubari <a.nesarnobari@gmail.com>
2022-03-30 02:03:52 +0430

    Update ConversionProblem.cpp

--- 5-bit-manipulation/ConversionProblem.cpp
+++ 5-bit-manipulation/ConversionProblem.cpp
@@ -16,10 +16,24 @@ int countBitsToConvert(int a, int b){
     return counter;
 }
 
+int countBitsToConvert2(int a, int b){
+    int counter=0;
+    auto aBits= bitset<32>(a).to_ulong();
+    auto bBits = bitset<32>(b).to_ulong();
+    while(aBits!=0 || bBits!=0){
+        if((aBits ^ bBits)&1){
+            counter++;
+        }
+        aBits>>=1;
+        bBits>>=1;
+    }
+    return counter;
+}
+
 int main(){
-    auto count = countBitsToConvert(36,8);
-    cout<< "36 ->" << bitset<8>(36) << endl;
-    cout<< "8 ->" << bitset<8>(8) << endl;
+    auto count = countBitsToConvert2(29,15);
+    cout<< "29 ->" << bitset<8>(29) << endl;
+    cout<< "15 ->" << bitset<8>(15) << endl;
     cout << "Bit flips to convert = " << count <<endl;
     return 0;
 }
